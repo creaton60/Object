df1a6d35175d7f34989cbc39804920701b409f70
creaton60 <lovmistake@gmail.com>
2020-01-09 01:01:49 +0900

    [update feature] model field 추가

--- src/main/java/study/book/object/ticket/model/Invitation.java
+++ src/main/java/study/book/object/ticket/model/Invitation.java
@@ -1,4 +1,13 @@
 package study.book.object.ticket.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
 public class Invitation {
+    private LocalDateTime when;
 }
--- src/main/java/study/book/object/ticket/model/Ticket.java
+++ src/main/java/study/book/object/ticket/model/Ticket.java
@@ -1,4 +1,11 @@
 package study.book.object.ticket.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
 public class Ticket {
+    private Long fee;
 }
