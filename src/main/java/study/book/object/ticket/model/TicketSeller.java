f2d1ac9efb77442b99a0ad26c61d00f6727a3b13
creaton60 <lovmistake@gmail.com>
2020-01-11 23:12:55 +0900

    [update feature] 관람객과 판매처 Model 을 자율적인 존재로 설계 변경

--- src/main/java/study/book/object/ticket/model/TicketSeller.java
+++ src/main/java/study/book/object/ticket/model/TicketSeller.java
@@ -1,15 +1,24 @@
 package study.book.object.ticket.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
 @ToString
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
