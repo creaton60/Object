8ed6f7cdc258f641a618111ca298c3a11d72467a
creaton60 <lovmistake@gmail.com>
2020-01-12 20:41:43 +0900

    [update feature] Bag, TicketOffice 까지 자유로운 모델로 변경

--- src/main/java/study/book/object/ticket/model/Audience.java
+++ src/main/java/study/book/object/ticket/model/Audience.java
@@ -7,12 +7,16 @@ import lombok.ToString;
 /**
  * 관람객 Model
  */
-@Getter @Setter
+@Setter
 @ToString
 public class Audience {
     private Bag bag;
 
     public Audience(Bag bag) {
         this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
     }
 }
--- src/main/java/study/book/object/ticket/model/TicketSeller.java
+++ src/main/java/study/book/object/ticket/model/TicketSeller.java
@@ -11,14 +11,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        if(audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.sellTicketTo(audience);
     }
 }
