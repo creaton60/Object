b508eb3742876c21482d45d70c05099ddf61bc12
creaton60 <lovmistake@gmail.com>
2020-01-12 22:58:55 +0900

    [update feature] 할인율, 할인량 정책 구현

--- src/main/java/study/book/object/movie/policy/AmountDiscountPolicy.java
+++ src/main/java/study/book/object/movie/policy/AmountDiscountPolicy.java
@@ -1,4 +1,19 @@
 package study.book.object.movie.policy;
 
-public class AmountDiscountPolicy {
+import study.book.object.movie.Money;
+import study.book.object.movie.Screening;
+import study.book.object.movie.condition.DiscountCondition;
+
+public class AmountDiscountPolicy extends DiscountPolicy{
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
 }
--- src/main/java/study/book/object/movie/policy/PercentDiscountPolicy.java
+++ src/main/java/study/book/object/movie/policy/PercentDiscountPolicy.java
@@ -1,4 +1,19 @@
 package study.book.object.movie.policy;
 
-public class PercentDiscountPolicy {
+import study.book.object.movie.Money;
+import study.book.object.movie.Screening;
+import study.book.object.movie.condition.DiscountCondition;
+
+public class PercentDiscountPolicy extends DiscountPolicy{
+    private double percent;
+
+    public PercentDiscountPolicy(double percent, DiscountCondition... conditions){
+        super(conditions);
+        this.percent = percent;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return screening.getMovieFee().times(percent);
+    }
 }
