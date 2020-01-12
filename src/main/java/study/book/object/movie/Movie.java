7e22cac3e2725f478348da98351be1fde16cb451
creaton60 <lovmistake@gmail.com>
2020-01-12 22:37:26 +0900

    [update feature] Movie 에 할인요금정책 기능 추가

--- src/main/java/study/book/object/movie/Movie.java
+++ src/main/java/study/book/object/movie/Movie.java
@@ -3,13 +3,25 @@ package study.book.object.movie;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import study.book.object.movie.policy.DiscountPolicy;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 
 @Getter @Setter
 @ToString
 public class Movie {
+    private String title;
+    private Duration runningTime;
     private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
 
     public Money calculateMovieFee(Screening screening) {
         return fee.minus(new Money(BigDecimal.ZERO));
