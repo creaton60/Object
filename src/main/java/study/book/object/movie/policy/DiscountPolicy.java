85fdcd0d229b9fbde268a64ece3f43567c63c43f
creaton60 <lovmistake@gmail.com>
2020-01-12 22:48:29 +0900

    [update feature] 할인에 필요한 추상메소드 구현

--- src/main/java/study/book/object/movie/policy/DiscountPolicy.java
+++ src/main/java/study/book/object/movie/policy/DiscountPolicy.java
@@ -1,4 +1,30 @@
 package study.book.object.movie.policy;
 
+import study.book.object.movie.Money;
+import study.book.object.movie.Screening;
+import study.book.object.movie.condition.DiscountCondition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class DiscountPolicy {
+    private List<DiscountCondition> conditions = new ArrayList<>();
+
+    public DiscountPolicy(DiscountCondition... conditions) {
+        this.conditions = Arrays.asList(conditions);
+    }
+
+    public Money calculateDiscountAmount(Screening screening) {
+        for(DiscountCondition each : conditions) {
+            if(each.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening);
+            }
+        }
+
+        return Money.ZERO;
+    }
+
+    abstract protected Money getDiscountAmount(Screening screening);
+
 }
