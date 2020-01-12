b3f9d1ad91553288faf4331498171335ef136877
creaton60 <lovmistake@gmail.com>
2020-01-12 22:22:11 +0900

    [update feature] 협력하는 객체 모델로 개발

--- src/main/java/study/book/object/movie/Movie.java
+++ src/main/java/study/book/object/movie/Movie.java
@@ -1,4 +1,17 @@
 package study.book.object.movie;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter @Setter
+@ToString
 public class Movie {
+    private Money fee;
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(new Money(BigDecimal.ZERO));
+    }
 }
