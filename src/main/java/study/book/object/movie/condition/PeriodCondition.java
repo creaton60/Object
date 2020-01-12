01b76fb88b4682b9277f81ef919cfb0071aae69f
creaton60 <lovmistake@gmail.com>
2020-01-12 22:53:37 +0900

    [update feature] 순번할인조건, 시간할인조건 구현

--- src/main/java/study/book/object/movie/condition/PeriodCondition.java
+++ src/main/java/study/book/object/movie/condition/PeriodCondition.java
@@ -1,4 +1,27 @@
 package study.book.object.movie.condition;
 
-public class PeriodCondition {
+import lombok.extern.slf4j.Slf4j;
+import study.book.object.movie.Screening;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Slf4j
+public class PeriodCondition implements DiscountCondition {
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                startTime.compareTo(screening.getStartTime().toLocalTime()) <=0 &&
+                endTime.compareTo(screening.getStartTime().toLocalTime()) >=0;
+    }
 }
