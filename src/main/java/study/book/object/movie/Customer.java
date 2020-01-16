94d2ed9eb12bad64fa6a8f8350f2dbab1961e40e
creaton60 <lovmistake@gmail.com>
2020-01-16 23:07:45 +0900

    [update feature] 고객 예약 스크린 모델링 변경

--- src/main/java/study/book/object/movie/Customer.java
+++ src/main/java/study/book/object/movie/Customer.java
@@ -1,4 +1,17 @@
 package study.book.object.movie;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
 public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
